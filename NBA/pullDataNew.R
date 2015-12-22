2780f1797cd648867d541f05bbc4b05c244b7d97
EC2 Default User <ec2-user@ip-10-111-168-213.ec2.internal>
2015-12-22 17:21:53 -0500

    added new metrics to full data set weekly report

--- NBA/pullDataNew.R
+++ NBA/pullDataNew.R
@@ -17,7 +17,7 @@ for (i in seq(along=tables)) {
   s2.away_team = n.away_team and s2.home_team = n.home_team and n.game_time = s2.mgt;"))
 
   } else {
-  	lDataFrames[[i]] <- dbGetQuery(conn=con, statement=paste("SELECT * FROM '", tables[[i]], "'", sep=""))
+        lDataFrames[[i]] <- dbGetQuery(conn=con, statement=paste("SELECT * FROM '", tables[[i]], "'", sep=""))
   }
   cat(tables[[i]], ":", i, "\n")
 }
@@ -30,6 +30,7 @@ boxscores <- lDataFrames[[which(tables == "NBAstats")]]
 lookup <- lDataFrames[[which(tables == "NBASBTeamLookup")]]
 nbafinal <- lDataFrames[[which(tables == "NBAfinalstats")]]
 seasontotals <- lDataFrames[[which(tables == "NBAseasontotals")]]
+
 
 b<-apply(boxscores[,3:5], 2, function(x) strsplit(x, "-"))
 boxscores$fgm <- do.call("rbind",b$fgma)[,1]
@@ -48,6 +49,16 @@ lookup$away_team <- lookup$sb_team
 lookup$home_team <- lookup$sb_team
 
 ## Total Lines
+lines$game_time<-as.POSIXlt(lines$game_time)
+lines<-lines[order(lines$home_team, lines$game_time),]
+lines$key <- paste(lines$away_team, lines$home_team, lines$game_date)
+
+# grabs the first line value after 'OFF'
+#res2 <- tapply(1:nrow(lines), INDEX=lines$key, FUN=function(idxs) idxs[lines[idxs,'line'] != 'OFF'][1])
+#first<-lines[res2[which(!is.na(res2))],]
+#lines <- first[,1:6]
+
+## Merge line data with lookup table
 la<-merge(lookup, lines, by="away_team")
 lh<-merge(lookup, lines, by="home_team")
 la$key <- paste(la$espn_abbr, la$game_date)
@@ -58,7 +69,15 @@ colnames(m3a)[49] <- "CoversTotalLineUpdateTime"
 colnames(m3h)[49] <- "CoversTotalLineUpdateTime"
 
 ## Halftime Lines
-##halflines <- halflines[-which(halflines$line == "OFF"),]
+halflines$game_time<-as.POSIXlt(halflines$game_time)
+halflines<-halflines[order(halflines$home_team, halflines$game_time),]
+halflines$key <- paste(halflines$away_team, halflines$home_team, halflines$game_date)
+
+# grabs first line value after 'OFF'
+#res2 <- tapply(1:nrow(halflines), INDEX=halflines$key, FUN=function(idxs) idxs[halflines[idxs,'line'] != 'OFF'][1])
+#first<-halflines[res2[which(!is.na(res2))],]
+#halflines <- first[,1:6]
+
 la2<-merge(lookup, halflines, by="away_team")
 lh2<-merge(lookup, halflines, by="home_team")
 la2$key <- paste(la2$espn_abbr, la2$game_date)
@@ -67,13 +86,105 @@ m3a2<-merge(m2, la2, by="key")
 m3h2<-merge(m2, lh2, by="key")
 colnames(m3a2)[49] <- "CoversHalfLineUpdateTime"
 colnames(m3h2)[49] <- "CoversHalfLineUpdateTime"
-
 l<-merge(m3a, m3a2, by=c("game_date.y", "away_team"))
-l<-l[match(m3a$key, l$key.y),]
+#l<-l[match(m3a$key, l$key.y),]
+m3a<-m3a[match(l$key.y, m3a$key),]
 m3a<-cbind(m3a, l[,94:96])
 l2<-merge(m3h, m3h2, by=c("game_date.y", "home_team"))
-l2<-l2[match(m3h$key, l2$key.y),]
+#l2<-l2[match(m3h$key, l2$key.y),]
+m3h<-m3h[match(l2$key.y, m3h$key),]
 m3h<-cbind(m3h, l2[,94:96])
+colnames(m3h)[44:45] <- c("home_team.x", "home_team.y")
+colnames(m3a)[40] <- "home_team"
+if(dim(m3a)[1] > 0){
+ m3a$hometeam <- FALSE
+ m3h$hometeam <- TRUE
+ m3h <- m3h[,1:53]
+}
+
+m3a <- unique(m3a)
+m3h <- unique(m3h)
+
+halftime_stats<-rbind(m3a,m3h)
+if(length(which(halftime_stats$game_id %in% names(which(table(halftime_stats$game_id) != 2))) > 0)){
+halftime_stats<-halftime_stats[-which(halftime_stats$game_id %in% names(which(table(halftime_stats$game_id) != 2)) ),]
+}
+#halftime_stats <- subset(halftime_stats, line.y != 'OFF')
+halftime_stats<-halftime_stats[which(!is.na(halftime_stats$line.y)),]
+halftime_stats<-halftime_stats[order(halftime_stats$game_id),]
+halftime_stats$CoversTotalLineUpdateTime <- as.character(halftime_stats$CoversTotalLineUpdateTime)
+halftime_stats$CoversHalfLineUpdateTime<-as.character(halftime_stats$CoversHalfLineUpdateTime)
+
+#diffs<-ddply(halftime_stats, .(game_id), transform, diff=pts.x[1] - pts.x[2])
+if(dim(halftime_stats)[1] > 0 ){
+halftime_stats$half_diff <-  rep(aggregate(pts ~ game_id, data=halftime_stats, FUN=diff)[,2] * -1, each=2)
+halftime_stats$line.y<-as.numeric(halftime_stats$line.y)
+halftime_stats$line <- as.numeric(halftime_stats$line)
+halftime_stats$mwt<-rep(aggregate(pts ~ game_id, data=halftime_stats, sum)[,2], each=2) + halftime_stats$line.y - halftime_stats$line
+half_stats <- halftime_stats[seq(from=2, to=dim(halftime_stats)[1], by=2),]
+} else {
+  return(data.frame(results="No Results"))
+}
+
+all <- rbind(m3a, m3h)
+all <- all[,-1]
+all$key <- paste(all$game_id, all$team.y)
+all<-all[match(unique(all$key), all$key),]
+
+colnames(all) <- c("GAME_ID","TEAM","HALF_FGM", "HALF_FGA", "HALF_3PM","HALF_3PA", "HALF_FTM","HALF_FTA","HALF_OREB", "HALF_DREB", "HALF_REB",
+"HALF_AST", "HALF_STL", "HALF_BLK", "HALF_TO", "HALF_PF", "HALF_PTS", "HALF_TIMESTAMP", "TEAM1", "TEAM2", "GAME_DATE","GAME_TIME",
+"REMOVE2","REMOVE3","SEASON_FGM","SEASON_FGA", "SEASON_FGP","SEASON_3PM", "SEASON_3PA", "SEASON_3PP", "SEASON_FTM","SEASON_FTA","SEASON_FTP",
+"SEASON_2PM", "SEASON_2PA", "SEASON_2PP","SEASON_PPS", "SEASON_AFG","REMOVE4", "REMOVE5", "REMOVE6", "REMOVE7","REMOVE8", "REMOVE9", "REMOVE10",
+"LINE", "SPREAD", "COVERS_UPDATE","LINE_HALF", "SPREAD_HALF", "COVERS_HALF_UPDATE", "HOME_TEAM", "REMOVE11")
+all <- all[,-grep("REMOVE", colnames(all))]
+
+## Add the season total stats
+colnames(seasontotals)[1] <- "TEAM"
+colnames(seasontotals)[2] <- "GAME_DATE"
+all$key <- paste(all$GAME_DATE, all$TEAM)
+seasontotals$key <- paste(seasontotals$GAME_DATE, seasontotals$TEAM)
+
+## HOME/AWAY gets screwed up in this merge
+#x<-merge(seasontotals, all, by=c("key"))
+x <- cbind(all, seasontotals[match(all$key, seasontotals$key),])
+#x<- x[,c(-1,-2, -16, -35)]
+final<-x[,c(1:57)]
+colnames(final)[47:57] <- c("SEASON_GP", "SEASON_PPG", "SEASON_ORPG", "SEASON_DEFRPG", "SEASON_RPG", "SEASON_APG", "SEASON_SPG", "SEASON_BGP",
+"SEASON_TPG", "SEASON_FPG", "SEASON_ATO")
+final<-final[order(final$GAME_DATE, decreasing=TRUE),]
+
+## match half stats that have 2nd half lines with final set
+f<-final[which(final$GAME_ID %in% half_stats$game_id),]
+f$mwt <- half_stats[match(f$GAME_ID, half_stats$game_id),]$mwt
+f$half_diff <- half_stats[match(f$GAME_ID, half_stats$game_id),]$half_diff
+f[,3:17] <- apply(f[,3:17], 2, function(x) as.numeric(as.character(x)))
+f[,23:37] <- apply(f[,23:37], 2, function(x) as.numeric(as.character(x)))
+f[,47:57] <- apply(f[,47:57], 2, function(x) as.numeric(as.character(x)))
+f[,58:59] <- apply(f[,58:59], 2, function(x) as.numeric(as.character(x)))
+
+## Team1 and Team2 Halftime Differentials
+f <- f[order(f$GAME_ID),]
+f$fg_percent <- ((f$HALF_FGM / f$HALF_FGA) - (f$SEASON_FGM / f$SEASON_FGA))
+f$FGM <- (f$HALF_FGM - (f$SEASON_FGM / f$SEASON_GP / 2))
+f$TPM <- (f$HALF_3PM - (f$SEASON_3PM / f$SEASON_GP / 2))
+f$FTM <- (f$HALF_FTM - (f$SEASON_FTM / f$SEASON_GP / 2 - 1))
+f$TO <- (f$HALF_TO - (f$SEASON_ATO / 2))
+f$OREB <- (f$HALF_OREB - (f$SEASON_ORPG / 2))
+
+## Cumulative Halftime Differentials
+f$COVERS_UPDATE<-as.character(f$COVERS_UPDATE)
+f$COVERS_HALF_UPDATE <- as.character(f$COVERS_HALF_UPDATE)
+
+f$chd_fg<-rep(aggregate(fg_percent ~ GAME_ID, data=f, function(x) sum(x) / 2)[,2], each=2)
+f$chd_fgm <- rep(aggregate(FGM ~ GAME_ID, data=f, function(x) sum(x) / 2)[,2], each=2)
+f$chd_tpm <- rep(aggregate(TPM ~ GAME_ID, data=f, function(x) sum(x) / 2)[,2], each=2)
+f$chd_ftm <- rep(aggregate(FTM ~ GAME_ID, data=f, function(x) sum(x) / 2)[,2], each=2)
+f$chd_to <- rep(aggregate(TO ~ GAME_ID, data=f, function(x) sum(x) / 2)[,2], each=2)
+f$chd_oreb <- rep(aggregate(OREB ~ GAME_ID, data=f, function(x) sum(x) / 2)[,2], each=2)
+
+## load nightly model trained on all previous data
+## load("~/sports/models/NBAhalftimeOversModel.Rdat")
+
 
 nbafinal$key <- paste(nbafinal$game_id, nbafinal$team)
 n<-apply(nbafinal[,3:5], 2, function(x) strsplit(x, "-"))
@@ -85,80 +196,29 @@ nbafinal$ftm <- do.call("rbind",n$ftma)[,1]
 nbafinal$fta <- do.call("rbind",n$ftma)[,2]
 nbafinal <- nbafinal[,c(1,2,17:22,6:16)]
 
-colnames(m3h)[44:45] <- c("home_team.x", "home_team.y")
-colnames(m3a)[40] <- "home_team"
-all <- rbind(m3a, m3h)
-all <- all[,-1]
-all$key <- paste(all$game_id, all$team.y)
-all<-all[match(unique(all$key), all$key),]
+f$key <- paste(f$GAME_ID, f$TEAM)
+f<-cbind(f, nbafinal[match(f$key, nbafinal$key),])
+f <- f[,-72:-73]
+colnames(f)[72:86]<-paste0("FINAL_", colnames(f)[72:86])
 
-final<-merge(nbafinal, all, by="key")
-final <- final[,-1]
+f<-f[order(f$GAME_ID),]
 
-colnames(final) <- c("GAME_ID","TEAM","FINAL_FGM","FINAL_FGA", "FINAL_3PM","FINAL_3PA","FINAL_FTM","FINAL_FTA","FINAL_OREB","FINAL_DREB","FINAL_REB",
-"FINAL_AST","FINAL_STL","FINAL_BLK","FINAL_TO","FINAL_PF","FINAL_PTS","FINAL_BOXSCORE_TIMESTAMP", "REMOVE0","REMOVE1","HALF_FGM", "HALF_FGA", "HALF_3PM", 
-"HALF_3PA", "HALF_FTM","HALF_FTA","HALF_OREB", "HALF_DREB", "HALF_REB", "HALF_AST", "HALF_STL", "HALF_BLK", "HALF_TO", "HALF_PF", "HALF_PTS",
-"HALF_TIMESTAMP", "TEAM1", "TEAM2", "GAME_DATE","GAME_TIME","REMOVE2","REMOVE3", "SEASON_FGM","SEASON_FGA","SEASON_FG%", "SEASON_3PM", "SEASON_3PA","SEASON_3P%", 
-"SEASON_FTM","SEASON_FTA","SEASON_FT%","SEASON_2PM", "SEASON_2PA", "SEASON_2P%", "SEASON_PPS", "SEASON_AFG", "REMOVE4","REMOVE5","REMOVE6","REMOVE7","REMOVE8","REMOVE9",
-"REMOVE10", "LINE", "SPREAD", "COVERS_UPDATE","LINE_HALF", "SPREAD_HALF", "COVERS_HALF_UPDATE")
-final <- final[,-grep("REMOVE", colnames(final))]
+f$team <- ""
+f[seq(from=1, to=dim(f)[1], by=2),]$team <- "TEAM1"
+f[seq(from=2, to=dim(f)[1], by=2),]$team <- "TEAM2"
 
-## Add the season total stats
-colnames(seasontotals)[1] <- "TEAM"
-colnames(seasontotals)[2] <- "GAME_DATE"
-#today <- format(Sys.Date(), "%m/%d/%Y")
-#seasontotals <- subset(seasontotals, GAME_DATE == today)
-final$key <- paste(final$GAME_DATE, final$TEAM)
-#seasontotals$TEAM<-match(seasontotals$TEAM, lookup$espn_name)
-seasontotals$key <- paste(seasontotals$GAME_DATE, seasontotals$TEAM)
 
-#x<-merge(seasontotals, final, by=c("key"))
-#x<- x[,c(-1, -16, -51)]
-#final<-x[,c(14:46, 1:13, 47:69)]
-#colnames(final)[36:46] <- c("SEASON_GP", "SEASON_PPG", "SEASON_ORPG", "SEASON_DEFR", "SEASON_RPG", "SEASON_APG", "SEASON_SPG", "SEASON_BPG", "SEASON_TPG", "SEASON_FPG", "SEASON_ATO")
-#final$GAME_DATE <- seasontotals$GAME_DATE[1]
-#final$GAME_DATE<-games[match(final$GAME_ID, games$game_id),]$game_date
+wide <- reshape(f, direction = "wide", idvar="GAME_ID", timevar="team")
 
-x <- cbind(final, seasontotals[match(final$key, seasontotals$key),])
-final<-x[,c(1:72)]
-colnames(final)[62:72] <- c("SEASON_GP", "SEASON_PPG", "SEASON_ORPG", "SEASON_DEFRPG", "SEASON_RPG", "SEASON_APG", "SEASON_SPG", "SEASON_BGP","SEASON_TPG", "SEASON_FPG", "SEASON_ATO")
-#final<-final[order(final$GAME_DATE, decreasing=TRUE),]
+result <- wide
+result$GAME_DATE<- strptime(paste(result$GAME_DATE.1.TEAM1, result$GAME_TIME.TEAM1), format="%m/%d/%Y %I:%M %p")
 
-final<-final[order(final$GAME_DATE, decreasing=TRUE),]
+colnames(result)[58] <- "MWT"
+colnames(result)[38] <- "SPREAD"
+colnames(result)[66:71] <- c("chd_fg", "chd_fgm", "chd_tpm", "chd_ftm", "chd_to", "chd_oreb")
+result$SPREAD <- as.numeric(result$SPREAD)
 
-final$LINE_HALF <- as.numeric(final$LINE_HALF)
-final$LINE <- as.numeric(final$LINE)
-final$COVERS_UPDATE<-as.character(final$COVERS_UPDATE)
-final<-ddply(final, .(GAME_ID), transform, mwt=HALF_PTS[1] + HALF_PTS[2] + LINE_HALF - LINE)
-final <- ddply(final, .(GAME_ID), transform, half_diff=HALF_PTS[1] - HALF_PTS[2])
-
-## transform to numerics
-final[,3:17]<-apply(final[,3:17], 2, as.numeric)
-final[,19:33]<-apply(final[,19:33], 2, as.numeric)
-final[,c(39:54)]<-apply(final[,c(39:54)], 2, as.numeric)
-final[,c(62:74)]<-apply(final[,c(62:74)], 2, as.numeric)
-
-## Team1 and Team2 Halftime Differentials
-final$fg_percent <- ((final$HALF_FGM / final$HALF_FGA) - (final$SEASON_FGM / final$SEASON_FGA) - .01)
-final$fg_percent_noadjustment <- (final$HALF_FGM / final$HALF_FGA) - (final$SEASON_FGM / final$SEASON_FGA)
-final$FGM <- (final$HALF_FGM - (final$SEASON_FGM / 2))
-final$TPM <- (final$HALF_3PM - (final$SEASON_3PM / 2))
-final$FTM <- (final$HALF_FTM - (final$SEASON_FTM /  2 - 1))
-final$TO <- (final$HALF_TO - (final$SEASON_ATO / 2))
-final$OREB <- (final$HALF_OREB - (final$SEASON_ORPG / 2))
-
-## Cumulative Halftime Differentials
-final$chd_fg <- ddply(final, .(GAME_ID), transform, chd_fg = (fg_percent[1] + fg_percent[2]) / 2)$chd_fg
-final$chd_fgm <- ddply(final, .(GAME_ID), transform, chd_fgm = (FGM[1] + FGM[2]) / 2)$chd_fgm
-final$chd_tpm <- ddply(final, .(GAME_ID), transform, chd_tpm = (TPM[1] + TPM[2]) / 2)$chd_tpm
-final$chd_ftm <- ddply(final, .(GAME_ID), transform, chd_ftm = (FTM[1] + FTM[2]) / 2)$chd_ftm
-final$chd_to <- ddply(final, .(GAME_ID), transform, chd_to = (TO[1] + TO[1]) / 2)$chd_to
-final$chd_oreb <- ddply(final, .(GAME_ID), transform, chd_oreb = (OREB[1] + OREB[2]) / 2)$chd_oreb
-
-result <- final
-
-## Add Criteria for Over/Under
-result$mwtO <- as.numeric(result$mwt < 7.1 & result$mwt > -3.9)
+result$mwtO <- as.numeric(result$MWT < 7.1 & result$MWT > -3.9)
 result$chd_fgO <- as.numeric(result$chd_fg < .15 & result$chd_fg > -.07)
 result$chd_fgmO <- as.numeric(result$chd_fgm < -3.9)
 result$chd_tpmO <- as.numeric(result$chd_tpm < -1.9)
@@ -173,8 +233,8 @@ result$chd_ftmO[is.na(result$chd_ftmO)] <- 0
 result$chd_toO[is.na(result$chd_toO)] <- 0
 result$overSum <- result$mwtO + result$chd_fgO + result$chd_fgmO + result$chd_tpmO + result$chd_ftmO + result$chd_toO
 
-result$fullSpreadU <- as.numeric(abs(as.numeric(result$SPREAD)) > 10.9)
-result$mwtU <- as.numeric(result$mwt > 7.1)
+result$fullSpreadU <- as.numeric(abs(result$SPREAD) > 10.9)
+result$mwtU <- as.numeric(result$MWT > 7.1)
 result$chd_fgU <- as.numeric(result$chd_fg > .15 | result$chd_fg < -.07)
 result$chd_fgmU <- 0
 result$chd_tpmU <- 0
@@ -189,35 +249,36 @@ result$chd_ftmU[is.na(result$chd_ftmU)] <- 0
 result$chd_toU[is.na(result$chd_toU)] <- 0
 result$underSum <- result$fullSpreadU + result$mwtU + result$chd_fgU + result$chd_fgmU + result$chd_tpmU + result$chd_ftmU + result$chd_toU
 
-result<-result[order(result$GAME_ID),]
-result$team <- ""
-result[seq(from=1, to=dim(result)[1], by=2),]$team <- "TEAM1"
-result[seq(from=2, to=dim(result)[1], by=2),]$team <- "TEAM2"
-wide<-reshape(result, direction = "wide", idvar="GAME_ID", timevar="team")
+result <- result[order(result$GAME_DATE),]
+result$GAME_DATE <- as.character(result$GAME_DATE)
+colnames(result)[67] <- 'chd_fg.TEAM1'
+load("~/sports2015/NBA/nbaRPartModel.Rdat")
 
-result$secondHalfPts.TEAM1 <- result$FINAL_PTS.TEAM1 - result$HALF_PTS.TEAM1
-result$secondHalfPts.TEAM2 <- result$FINAL_PTS.TEAM2 - result$HALF_PTS.TEAM2
-result$secondHalfPtsTotal <- result$secondHalfPts.TEAM1 + result$secondHalfPts.TEAM2
-result$Over<-result$secondHalfPtsTotal > result$LINE_HALF.TEAM1
-result <- result[-which(is.na(result$Over)),]
+#colnames(result)[120] <- "mwt.TEAM1"
+colnames(result)[which(colnames(result) == 'chd_fgm')] <- 'chd_fgm.TEAM1'
+colnames(result)[which(colnames(result) == 'chd_fg')] <- 'chd_fg.TEAM1'
+colnames(result)[which(colnames(result) == 'chd_ftm')] <- 'chd_ftm.TEAM1'
+colnames(result)[which(colnames(result) == 'chd_to')] <- 'chd_to.TEAM1'
+colnames(result)[which(colnames(result) == 'chd_oreb')] <- 'chd_oreb.TEAM1'
+colnames(result)[which(colnames(result) == 'chd_tpm')] <- 'chd_tpm.TEAM1'
+
 
 result$SPREAD_HALF.TEAM1<-as.numeric(result$SPREAD_HALF.TEAM1)
 
 result$FGS_GROUP <- NA
-if(length(which(abs(result$SPREAD.TEAM1) < 3.1)) > 0){
-result[which(abs(result$SPREAD.TEAM1) < 3.1),]$FGS_GROUP <- '1'
+if(length(which(abs(result$SPREAD) < 3.1)) > 0){
+result[which(abs(result$SPREAD) < 3.1),]$FGS_GROUP <- '1'
 }
-if(length(which(abs(result$SPREAD.TEAM1) >= 3.1 & abs(result$SPREAD.TEAM1) < 8.1)) > 0){
-result[which(abs(result$SPREAD.TEAM1) >= 3.1 & abs(result$SPREAD.TEAM1) < 8.1),]$FGS_GROUP <- '2'
+if(length(which(abs(result$SPREAD) >= 3.1 & abs(result$SPREAD) < 8.1)) > 0){
+result[which(abs(result$SPREAD) >= 3.1 & abs(result$SPREAD) < 8.1),]$FGS_GROUP <- '2'
 }
-if(length(which(abs(result$SPREAD.TEAM1) >= 8.1)) > 0){
-result[which(abs(result$SPREAD.TEAM1) >= 8.1),]$FGS_GROUP <- '3'
+if(length(which(abs(result$SPREAD) >= 8.1)) > 0){
+result[which(abs(result$SPREAD) >= 8.1),]$FGS_GROUP <- '3'
 }
 
 result$LINE_HALF.TEAM1<-as.numeric(result$LINE_HALF.TEAM1)
 result$HALF_DIFF <- NA
-result$HOME_TEAM.TEAM1 <- FALSE
-result$underDog.TEAM1 <- result$HOME_TEAM.TEAM1 == FALSE & result$SPREAD.TEAM1 > 0
+result$underDog.TEAM1 <- result$HOME_TEAM.TEAM1 == FALSE & result$SPREAD > 0
 under.teams <- which(result$underDog.TEAM1)
 favorite.teams <- which(!result$underDog.TEAM1)
 result[under.teams,]$HALF_DIFF <- result[under.teams,]$HALF_PTS.TEAM2 - result[under.teams,]$HALF_PTS.TEAM1
@@ -234,7 +295,6 @@ result[under.teams,]$POSSvE <- ((result[under.teams,]$possessions.TEAM2 + result
                                 2 + result[under.teams,]$possessions.TEAM1.SEASON / 2) / 2)
 result[favorite.teams,]$POSSvE <- ((result[favorite.teams,]$possessions.TEAM1 + result[favorite.teams,]$possessions.TEAM2) / 2) - ((result[favorite.teams,]$possessions.TEAM1.SEASON /
                                 2 + result[favorite.teams,]$possessions.TEAM2.SEASON / 2) / 2)
-
 result$P100vE <- NA
 result$P100.TEAM1 <- result$HALF_PTS.TEAM1 / result$possessions.TEAM1 * 100
 result$P100.TEAM1.SEASON <- result$SEASON_PPG.TEAM1 / result$possessions.TEAM1.SEASON * 100
@@ -254,14 +314,23 @@ result$FAV <- ""
 result[which(result$underDog.TEAM1),]$FAV <- result[which(result$underDog.TEAM1),]$TEAM2.TEAM2
 result[which(!result$underDog.TEAM1),]$FAV <- result[which(!result$underDog.TEAM1),]$TEAM1.TEAM1
 result$MWTv3 <- 0
-if(length(which(result$TEAM2.TEAM2 == result$FAV)) > 0){
-        i <- which(result$TEAM2.TEAM2 == result$FAV)
-        result$MWTv3[i] <- abs(result[i,]$SPREAD_HALF.TEAM1) - (abs(result[i,]$SPREAD.TEAM1) / 2)
+if(length(which(result$HOME_TEAM.TEAM2 & result$TEAM2.TEAM2 == result$FAV)) > 0){
+        i <- which(result$HOME_TEAM.TEAM2 & result$TEAM2.TEAM2 == result$FAV)
+        result$MWTv3[i] <- abs(result[i,]$SPREAD_HALF.TEAM1) - (abs(result[i,]$SPREAD) / 2)
 }
 if (length(which(result$TEAM2.TEAM2 != result$FAV)) > 0){
         i <- which(result$TEAM2.TEAM2 != result$FAV)
-        result$MWTv3[i] <- -abs(result[i,]$SPREAD_HALF.TEAM1) - (-abs(result[i,]$SPREAD.TEAM1) / 2)
+        result$MWTv3[i] <- -abs(result[i,]$SPREAD_HALF.TEAM1) - (-abs(result[i,]$SPREAD) / 2)
 }
+
+
+## Need to merge nba final data first
+#result <- cbind(result,nbafinal[match(result$GAME_ID, nbafinal$GAME_ID),])
+#result$secondHalfPts.TEAM1 <- result$FINAL_PTS.TEAM1 - result$HALF_PTS.TEAM1
+#result$secondHalfPts.TEAM2 <- result$FINAL_PTS.TEAM2 - result$HALF_PTS.TEAM2
+#result$secondHalfPtsTotal <- result$secondHalfPts.TEAM1 + result$secondHalfPts.TEAM2
+#result$Over<-result$secondHalfPtsTotal > result$LINE_HALF.TEAM1
+#result <- result[-which(is.na(result$Over)),]
 
 write.csv(result, file="/home/ec2-user/sports2015/NBA/sportsbook.csv", row.names=FALSE)
 
@@ -276,4 +345,5 @@ body <- c(
   mime_part("/home/ec2-user/sports2015/NBA/sportsbook.csv", "WeeklyDataNBA_SB.csv")
 )
 sendmailV(from, to=emails, subject, body)
+
 
