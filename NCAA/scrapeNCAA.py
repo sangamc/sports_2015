62e9df9d7fe2ab223c42a1b835e704fd91c0dfb1
EC2 Default User <ec2-user@ip-10-186-62-220.ec2.internal>
2016-02-19 12:02:38 -0500

    fixes for redesign

--- NCAA/scrapeNCAA.py
+++ NCAA/scrapeNCAA.py
@@ -7,6 +7,7 @@ import time
 import re
 import random
 from datetime import date, timedelta
+from dateutil import tz
 import datetime
 import os
 import sqlite3
@@ -15,6 +16,9 @@ from urlparse import urlparse
 from bs4 import BeautifulSoup as bs
 
 db = sqlite3.connect('/home/ec2-user/sports2015/NCAA/sports.db')
+
+from_zone = tz.gettz('UTC')
+to_zone = tz.gettz('America/New_York')
 
 x=random.randint(4, 10)
 time.sleep(x)
@@ -29,103 +33,85 @@ def index():
     halftime_ids = []
     today = date.today()
     today = today.strftime("%Y%m%d")
-    #vals = [50,3,46,2,1,62,8,4,5,6,7,9,11,10,45,12,13,14,16,18,44,19,20,21,22,23,26,24,25,49,27,30,29]
-    vals = [50]
-    for v in range(0,len(vals)):
-        url = urllib2.urlopen('http://scores.espn.go.com/mens-college-basketball/scoreboard/_/group/' + str(vals[v]) + '/date/' + today)
-        soup = bs(url.read(), ['fast', 'lxml'])
-        data=re.search('window.espn.scoreboardData.*{(.*)};</script>', str(soup)).group(0)
-        jsondata=re.search('({.*});window', data).group(1)
-        j=jsonpickle.decode(jsondata)
-        games=j['events']
-        status = [game['status'] for game in games]
-        half = [s['type']['shortDetail'] for s in status]
-        index = [i for i, j in enumerate(half) if j == 'Halftime']
-        ids = [game['id'] for game in games]
-        halftime_ids = [j for k, j in enumerate(ids) if k in index]
-        halftime_ids = list(set(halftime_ids) - set(game_ids))
-        print len(halftime_ids)
-        league = 'ncaa'
-        if(len(halftime_ids) == 0):
-            print "No Halftime Box Scores yet."
-        else:
-            for i in range(0, len(halftime_ids)):
-                x=random.randint(2, 5)
-                time.sleep(x)
-                espn = 'http://espn.go.com/mens-college-basketball/boxscore?gameId=' + halftime_ids[i]
-                url = urllib2.urlopen(espn)
-                soup = bs(url.read(), ['fast', 'lxml'])
-                game_date = soup.findAll('div', {'class':'game-time-location'})[0].p.text
-                the_date =  re.search(',\s(.*)', game_date).group(1)
-                the_time = re.search('^(.*?),', game_date)
-                game_time = the_time.group(1)
-                t=time.strptime(the_date, "%B %d, %Y")
-                gdate=time.strftime('%m/%d/%Y', t)
-                boxscore = soup.find('table', {'class':'mod-data'})
-                try: 
-                    theads=boxscore.findAll('thead')
-                    the_thead = None
-                    for thead in theads:
-                        ths=thead.findAll('th')
-                        labels = [x.text for x in ths]
-                        if (any("TOTALS" in s for s in labels)):
-                            the_thead = thead
-                            break
-                    headers = the_thead.findAll('th')
-                    header_vals = [h.text for h in headers]
-                    team1_data = the_thead.nextSibling
-                    tds = team1_data.findAll('td')
-                    values = [v.text for v in tds]
-                    remove = [v == '' for v in values]
-                    remove_indices = [a for a, b in enumerate(remove) if b]
-                    cleaned1 = [j for k, j in enumerate(values) if k not in remove_indices]
-                    linescore = soup.find('table', {'class':'linescore'})
-                    team1 = linescore.findAll('a')[0].text
-                    team2 = linescore.findAll('a')[1].text
-                    if team1 == 'WM':
-                        team1 = 'W&M'
-                    if team2 == 'WM':
-                        team2 = 'W&M'
-                    if team1 == 'TAM':
-                        team1 = 'TA&M'
-                    if team2 == 'TAM':
-                        team2 = 'TA&M'
-                    team1_data = the_thead.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling
-                    tds = team1_data.findAll('td')
-                    values = [v.text for v in tds]
-                    remove = [v == '' for v in values]
-                    remove_indices = [j for j, x in enumerate(remove) if x]
-                    cleaned2 = [j for k, j in enumerate(values) if k not in remove_indices]
-                    cleaned1.insert(0, team1)
-                    cleaned2.insert(0, team2)
-                    cleaned1.insert(0, halftime_ids[i])
-                    cleaned2.insert(0, halftime_ids[i])
-                    cleaned1.pop()
-                    cleaned1.pop()
-                    cleaned1.pop()
-                    cleaned2.pop()
-                    cleaned2.pop()
-                    cleaned2.pop()
-                    try:
-                        with db:
-                            db.execute('''INSERT INTO NCAAgames(game_id, team1, team2, game_date, game_time) VALUES(?,?,?,?,?)''', (halftime_ids[i], team1, team2, gdate, game_time))
-                            db.commit()
-                        try:
-                            date_time = str(datetime.datetime.now())
-                            db.execute('''INSERT INTO NCAAstats(game_id, team, fgma, tpma, ftma, oreb, dreb, reb, ast, stl, blk, turnovers, pf, pts, timestamp ) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (halftime_ids[i], team1, cleaned1[2], cleaned1[3], cleaned1[4], int(cleaned1[5]), int(cleaned1[6]), int(cleaned1[7]), int(cleaned1[8]), int(cleaned1[9]), int(cleaned1[10]), int(cleaned1[11]), int(cleaned1[12]), int(cleaned1[13]), date_time))
-                            db.commit()
-                        except sqlite3.IntegrityError:
-                            print sqlite3.Error
-                        try:
-                            date_time = str(datetime.datetime.now())
-                            db.execute('''INSERT INTO NCAAstats(game_id, team, fgma, tpma, ftma, oreb, dreb, reb, ast, stl, blk, turnovers, pf, pts, timestamp ) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (halftime_ids[i], team2, cleaned2[2], cleaned2[3], cleaned2[4], int(cleaned2[5]), int(cleaned2[6]), int(cleaned2[7]), int(cleaned2[8]), int(cleaned2[9]), int(cleaned2[10]), int(cleaned2[11]), int(cleaned2[12]), int(cleaned2[13]), date_time))
-                            db.commit()
-                        except sqlite3.IntegrityError:
-                            print sqlite3.Error            
-                    except sqlite3.IntegrityError:
-                        print 'Record Already Exists'    
-                except:
-                    print 'No Boxscore Available'     
+    url = urllib2.urlopen('http://scores.espn.go.com/mens-college-basketball/scoreboard/_/group/50' + '/date/' + today)
+    soup = bs(url.read(), ['fast', 'lxml'])
+    data=re.search('window.espn.scoreboardData.*{(.*)};</script>', str(soup)).group(0)
+    jsondata=re.search('({.*});window', data).group(1)
+    j=jsonpickle.decode(jsondata)
+    games=j['events']
+    status = [game['status'] for game in games]
+    half = [s['type']['shortDetail'] for s in status]
+    index = [i for i, j in enumerate(half) if j == 'Halftime']
+    ids = [game['id'] for game in games]
+    halftime_ids = [j for k, j in enumerate(ids) if k in index]
+    halftime_ids = list(set(halftime_ids) - set(game_ids))
+    print len(halftime_ids)
+    league = 'ncaa'
+    if(len(halftime_ids) == 0):
+        print "No Halftime Box Scores yet."
+    else:
+        for i in range(0, len(halftime_ids)):
+            x=random.randint(2, 5)
+            time.sleep(x)
+            espn = 'http://scores.espn.go.com/mens-college-basketball/game?gameId=' + halftime_ids[i]
+            url = urllib2.urlopen(espn)
+            soup = bs(url.read(), ['fast', 'lxml'])
+
+            game_date = soup.findAll('div', {'class':'game-date-time'})[0]
+            the_date = re.search('(\\d{4}\\-\\d{2}\\-\\d{2})', str(game_date)).group(1)
+            game_time = re.search('T(.*?)Z', str(game_date)).group(1)            
+            the_utc = the_date + ' ' + game_time
+            t=datetime.strptime(the_utc, "%Y-%m-%d %H:%M")
+            t = t.replace(tzinfo=from_zone)
+            est = t.astimezone(to_zone)
+            gdate = est.strftime('%m/%d/%Y')
+            game_time =  est.strftime('%H:%M') + ' PM ET'
+
+            espn = 'http://scores.espn.go.com/mens-college-basketball/boxscore?gameId=' + halftime_ids[i]
+            url = urllib2.urlopen(espn)
+            soup = bs(url.read(), ['fast', 'lxml'])
+            try:
+                boxscore = soup.findAll('table', {'class':'mod-data'})
+                highlight1 = boxscore[0].findAll('tr', {'class':'highlight'})
+                highlight2 = boxscore[1].findAll('tr', {'class':'highlight'}) 
+                team1_tds=highlight1[0].findAll('td')
+                team2_tds=highlight2[0].findAll('td')           
+                tds1 = [td.text for td in team1_tds]
+                tds2 = [td.text for td in team2_tds]
+                team1 = soup.findAll('span', {'class':'abbrev'})[0].text
+                team2 = soup.findAll('span', {'class':'abbrev'})[1].text
+
+                if team1 == 'WM':
+                    team1 = 'W&M'
+                if team2 == 'WM':
+                    team2 = 'W&M'
+                if team1 == 'TAM':
+                    team1 = 'TA&M'
+                if team2 == 'TAM':
+                    team2 = 'TA&M'
+            except:
+                print "Problem with boxscore, continuing to next game."
+                continue
+            try:
+                with db:
+                    db.execute('''INSERT INTO NCAAgames(game_id, team1, team2, game_date, game_time) VALUES(?,?,?,?,?)''', (halftime_ids[i], team1, team2, gdate, game_time))
+                    db.commit()
+                try:
+                    date_time = str(datetime.datetime.now())
+                    db.execute('''INSERT INTO NCAAstats(game_id, team, fgma, tpma, ftma, oreb, dreb, reb, ast, stl, blk, turnovers, pf, pts, timestamp ) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (halftime_ids[i], team1, tds1[2], tds1[3], tds1[4], int(tds1[5]), int(tds1[6]), int(tds1[7]), int(tds1[8]), int(tds1[9]), int(tds1[10]), int(tds1[11]), int(tds1[12]), int(tds1[13]), date_time))
+                    db.commit()
+                except sqlite3.IntegrityError:
+                    print sqlite3.Error
+                try:
+                    date_time = str(datetime.datetime.now())
+                    db.execute('''INSERT INTO NCAAstats(game_id, team, fgma, tpma, ftma, oreb, dreb, reb, ast, stl, blk, turnovers, pf, pts, timestamp ) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (halftime_ids[i], team2, tds2[2], tds2[3], tds2[4], int(tds2[5]), int(tds2[6]), int(tds2[7]), int(tds2[8]), int(tds2[9]), int(tds2[10]), int(tds2[11]), int(tds2[12]), int(tds2[13]), date_time))
+                    db.commit()
+                except sqlite3.IntegrityError:
+                    print sqlite3.Error            
+                except sqlite3.IntegrityError:
+                    print 'Record Already Exists'    
+            except:
+                print 'No Boxscore Available'     
 index()
 db.close()
 
--- NCAA/scrapeNCAAfinal.py
+++ NCAA/scrapeNCAAfinal.py
@@ -31,101 +31,65 @@ def index():
     final_ids = []
     today = date.today() - timedelta(days=1)
     today = today.strftime("%Y%m%d")
-    #vals = [50,3,46,2,1,62,8,4,5,6,7,9,11,10,45,12,13,14,16,18,44,19,20,21,22,23,26,24,25,49,27,30,29]
-    vals = [50]
-    for v in range(0, len(vals)):
-        url = urllib2.urlopen('http://scores.espn.go.com/mens-college-basketball/scoreboard/_/group/' + str(vals[v]) + '/date/' + today)
-        x=random.randint(2, 4)
-        time.sleep(x)
-        soup = bs(url.read(), ['fast', 'lxml'])
-        print "entered index" + "conf=" + str(vals[v])
-        final_ids = re.findall('http://espn.go.com/mens-college-basketball/boxscore\?gameId=(\d+)', str(soup))
-        final_ids = list(set(final_ids) - set(game_ids)) 
-        print len(final_ids)
-        if(len(final_ids) == 0):
-            print "No Final Box Scores yet or already recorded."
-        else:
-            for i in range(0, len(final_ids)):
-                x=random.randint(3,10)
-                time.sleep(x)
-                espn = 'http://espn.go.com/mens-college-basketball/boxscore?gameId=' + final_ids[i]
-                while True:
-                    try:
-                        url1 = urllib2.urlopen(espn)
-                    except:
-                        time.sleep(x)
-                        continue
-                    break
-                soup = bs(url1.read(), ['fast', 'lxml'])
-                game_date = soup.findAll('div', {'class':'game-time-location'})[0].p.text
-                the_date =  re.search(',\s(.*)', game_date).group(1)
-                the_time = re.search('^(.*?),', game_date)
-                game_time = the_time.group(1)
-                t=time.strptime(the_date, "%B %d, %Y")
-                gdate=time.strftime('%m/%d/%Y', t)
-                boxscore = soup.find('table', {'class':'mod-data'})
-                try: 
-                    theads=boxscore.findAll('thead')
-                    the_thead = None
-                    for thead in theads:
-                        ths=thead.findAll('th')
-                        labels = [x.text for x in ths]
-                        if (any("TOTALS" in s for s in labels)):
-                            the_thead = thead
-                            break
-                    headers = the_thead.findAll('th')
-                    header_vals = [h.text for h in headers]
-                    team1_data = the_thead.nextSibling
-                    tds = team1_data.findAll('td')
-                    values = [v.text for v in tds]
-                    remove = [v == '' for v in values]
-                    remove_indices = [a for a, b in enumerate(remove) if b]
-                    cleaned1 = [j for k, j in enumerate(values) if k not in remove_indices]
-                    linescore = soup.find('table', {'class':'linescore'})
-                    team1 = linescore.findAll('a')[0].text
-                    team2 = linescore.findAll('a')[1].text
-                    if team1 == 'WM':
-                        team1 = 'W&M'
-                    if team2 == 'WM':
-                        team2 = 'W&M'
-                    if team1 == 'TAM':
-                        team1 = 'TA&M'
-                    if team2 == 'TAM':
-                        team2 = 'TA&M'
-                    team1_data = the_thead.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling
-                    tds = team1_data.findAll('td')
-                    values = [v.text for v in tds]
-                    remove = [v == '' for v in values]
-                    remove_indices = [j for j, x in enumerate(remove) if x]
-                    cleaned2 = [j for k, j in enumerate(values) if k not in remove_indices]
-                    cleaned1.insert(0, team1)
-                    cleaned2.insert(0, team2)
-                    cleaned1.insert(0, final_ids[i])
-                    cleaned2.insert(0, final_ids[i])
-                    cleaned1.pop()
-                    cleaned1.pop()
-                    cleaned1.pop()
-                    cleaned2.pop()
-                    cleaned2.pop()
-                    cleaned2.pop()
-                    try:
-                        date_time = str(datetime.now())
-                        db.execute('''INSERT INTO NCAAfinalstats(game_id, team, fgma, tpma, ftma, oreb, dreb, reb, ast, stl, blk, turnovers, pf, pts, timestamp ) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (final_ids[i], team1, cleaned1[2], cleaned1[3], cleaned1[4], int(cleaned1[5]), int(cleaned1[6]), int(cleaned1[7]), int(cleaned1[8]), int(cleaned1[9]), int(cleaned1[10]), int(cleaned1[11]), int(cleaned1[12]), int(cleaned1[13]), date_time))
-                        db.commit()
-                    except:
-                        print sys.exc_info()[0]
-                        pass
-                    try:
-                        date_time = str(datetime.now())
-                        db.execute('''INSERT INTO NCAAfinalstats(game_id, team, fgma, tpma, ftma, oreb, dreb, reb, ast, stl, blk, turnovers, pf, pts, timestamp ) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (final_ids[i], team2, cleaned2[2], cleaned2[3], cleaned2[4], int(cleaned2[5]), int(cleaned2[6]), int(cleaned2[7]), int(cleaned2[8]), int(cleaned2[9]), int(cleaned2[10]), int(cleaned2[11]), int(cleaned2[12]), int(cleaned2[13]), date_time))
-                        db.commit()
-                    except:
-                        print sys.exc_info()[0]
-                        pass
+    url = urllib2.urlopen('http://scores.espn.go.com/mens-college-basketball/scoreboard/_/group/50/date/' + today)
+    x=random.randint(2, 4)
+    time.sleep(x)
+    soup = bs(url.read(), ['fast', 'lxml'])
+    final_ids = re.findall('http://espn.go.com/mens-college-basketball/game\?gameId=(\d+)', str(soup))
+    final_ids = list(set(final_ids) - set(game_ids)) 
+    print len(final_ids)
+    if(len(final_ids) == 0):
+        print "No Final Box Scores yet or already recorded."
+    else:
+        for i in range(0, len(final_ids)):
+            print final_ids[i]
+            x=random.randint(3,10)
+            time.sleep(x)
+            espn = 'http://espn.go.com/mens-college-basketball/boxscore?gameId=' + final_ids[i]
+            while True:
+                try:
+                    url1 = urllib2.urlopen(espn)
                 except:
-                    print sys.exc_info()[0]
-                    pass
+                    time.sleep(x)
+                    continue
+                break
+            soup = bs(url1.read(), ['fast', 'lxml'])
+            try:
+                boxscore = soup.findAll('table', {'class':'mod-data'})
+                highlight1 = boxscore[0].findAll('tr', {'class':'highlight'})
+                highlight2 = boxscore[1].findAll('tr', {'class':'highlight'}) 
+                team1_tds=highlight1[0].findAll('td')
+                team2_tds=highlight2[0].findAll('td')           
+                tds1 = [td.text for td in team1_tds]
+                tds2 = [td.text for td in team2_tds]
+                team1 = soup.findAll('span', {'class':'abbrev'})[0].text
+                team2 = soup.findAll('span', {'class':'abbrev'})[1].text
 
+                if team1 == 'WM':
+                    team1 = 'W&M'
+                if team2 == 'WM':
+                    team2 = 'W&M'
+                if team1 == 'TAM':
+                    team1 = 'TA&M'
+                if team2 == 'TAM':
+                    team2 = 'TA&M'
+            except:
+                print "Problems getting boxscore, continuing to next game."
+                continue            
+            try:
+                date_time = str(datetime.now())
+                db.execute('''INSERT INTO NCAAfinalstats(game_id, team, fgma, tpma, ftma, oreb, dreb, reb, ast, stl, blk, turnovers, pf, pts, timestamp ) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (final_ids[i], team1, tds1[2], tds1[3], tds1[4], int(tds1[5]), int(tds1[6]), int(tds1[7]), int(tds1[8]), int(tds1[9]), int(tds1[10]), int(tds1[11]), int(tds1[12]), int(tds1[13]), date_time))
+                db.commit()
+            except:
+                print sys.exc_info()[0]
+                pass
+            try:
+                date_time = str(datetime.now())
+                db.execute('''INSERT INTO NCAAfinalstats(game_id, team, fgma, tpma, ftma, oreb, dreb, reb, ast, stl, blk, turnovers, pf, pts, timestamp ) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (final_ids[i], team2, tds2[2], tds2[3], tds2[4], int(tds2[5]), int(tds2[6]), int(tds2[7]), int(tds2[8]), int(tds2[9]), int(tds2[10]), int(tds2[11]), int(tds2[12]), int(tds2[13]), date_time))
+                db.commit()
+            except:
+                print sys.exc_info()[0]
+                pass
 
 ## used for historical scraping
 #dtstart = datetime(2014,12,18)
