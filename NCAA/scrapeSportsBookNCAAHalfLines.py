33841c82fe233f9a7c44a29de5e8ac0dd15fcd94
EC2 Default User <ec2-user@ip-10-150-148-209.ec2.internal>
2016-03-17 23:35:29 -0400

    fix for sportsbook future lines

--- NCAA/scrapeSportsBookNCAAHalfLines.py
+++ NCAA/scrapeSportsBookNCAAHalfLines.py
@@ -19,6 +19,12 @@ time.sleep(x)
 
 url = urllib2.urlopen('https://www.sportsbook.ag/sbk/sportsbook4/ncaa-basketball-betting/2nd-half-ncaa-basketball.sbk')
 soup = bs(url.read(), ['fast', 'lxml'])
+
+#the_date = soup.findAll('div', {'class':'col-xs-12 date'})
+#the_date = the_date[0].text.strip()
+#today1 = str(date.today())
+#today1 = time.strftime("%B %d, %Y", time.strptime(today1, '%Y-%m-%d'))
+
 divs=soup.findAll('div', {'class':'col-sm-12 eventbox'})
 awayTeams=[d.findAll('span', {'id':'firstTeamName'}) for d in divs]
 homeTeams=[d.findAll('span', {'id':'secondTeamName'}) for d in divs]
@@ -43,7 +49,6 @@ for i in range(0, len(the_lines)):
     except:
         next
 
-
 #for i in range(0, len(the_lines)):
 #    try:
 #        lines.append(re.search('(\d+\\.?\d+)\\(', the_lines[i]).group(1))
@@ -52,6 +57,7 @@ for i in range(0, len(the_lines)):
 #        next
 
 #today = str(datetime.datetime.now() - timedelta(hours=2))[0:10]
+    
 today = str(date.today())
 today = time.strftime("%m/%d/%Y", time.strptime(today, '%Y-%m-%d'))
 
